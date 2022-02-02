a175ab56d0171edf65374fef64057a46be7ef0ee
Tambanco <52546246+Tambanco@users.noreply.github.com>
2022-02-02 21:08:40 +0300

    Add xcproj files
    Single view app

---
+++ XIB-to-Swift-code/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  XIB-to-Swift-code
+//
+//  Created by tambanco 🥳 on 02.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
