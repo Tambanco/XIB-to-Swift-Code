13bb194038cf388f11650b7a35491ff937d47148
Tambanco <52546246+Tambanco@users.noreply.github.com>
2022-02-03 12:42:37 +0300

    Update CustomCell.swift

--- XIB-to-Swift-code/MainModule/View/CustomCell.swift
+++ XIB-to-Swift-code/MainModule/View/CustomCell.swift
@@ -20,7 +20,7 @@ class CustomCell: UITableViewCell {
     
     let titleLabel: UILabel = {
         let label = UILabel()
-        label.font = UIFont(name: "System", size: 27)
+        label.font = UIFont(name: label.font.fontName, size: 27)
         return label
     }()
     
